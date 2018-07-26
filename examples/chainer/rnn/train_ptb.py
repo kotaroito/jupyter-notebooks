ff01e6b9e679df5aaa820fb57b025c1270e69f9e
kotaroito <kotaroito@gmail.com>
2018-07-27 08:39:42 +0900

    Initital commit

---
+++ examples/chainer/rnn/train_ptb.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from chainer import training
+from chainer.training import extensions
+
+print("Hello RNN")
