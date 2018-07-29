29d82c24c0fa80254d96cf876e70d8349602d797
kotaroito <kotaroito@gmail.com>
2018-07-30 08:18:58 +0900

    Add train_ptb

--- examples/chainer/rnn/train_ptb.py
+++ examples/chainer/rnn/train_ptb.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 
 import chainer
 import chainer.functions as F
@@ -6,4 +7,31 @@ import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
-print("Hello RNN")
+
+parser = argparse.ArgumentParser(description='RNN Language Model')
+parser.add_argument('--batchsize', '-b', type=int, default=20,
+                    help='Number of examples in each mini-batch')
+parser.add_argument('--bproplen', '-l', type=int, default=35,
+                    help='Number of words in each mini-batch '
+                         '(= length of truncated BPTT)')
+parser.add_argument('--epoch', '-e', type=int, default=39,
+                    help='Number of sweeps over the dataset to train')
+parser.add_argument('--gpu', '-g', type=int, default=-1,
+                    help='GPU ID (negative value indicates CPU)')
+parser.add_argument('--gradclip', '-c', type=float, default=5,
+                    help='Gradient norm threshold to clip')
+parser.add_argument('--out', '-o', default='result',
+                    help='Directory to output the result')
+parser.add_argument('--resume', '-r', default='',
+                    help='Resume the training from snapshot')
+parser.add_argument('--test', action='store_true',
+                    help='Use tiny datasets for quick tests')
+parser.set_defaults(test=False)
+parser.add_argument('--unit', '-u', type=int, default=650,
+                    help='Number of LSTM units in each layer')
+parser.add_argument('--model', '-m', default='model.npz',
+                    help='Model file name to serialize')
+
+args = parser.parse_args()
+
+print(args)
