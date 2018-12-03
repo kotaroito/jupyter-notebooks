ba070cd7590971878508f337b9eca04614326c1b
kotaroito <kotaroito@gmail.com>
2018-12-03 11:32:38 +0900

    Update script

--- scripts/datasets/caltech101/random_split_train_valid_test.py
+++ scripts/datasets/caltech101/random_split_train_valid_test.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import glob
 import random
+import shutil
 
 def random_split(images):
     random.shuffle(images)
@@ -27,22 +28,27 @@ def main():
     random.seed(args.seed)
 
     outdir = os.path.abspath(args.out)
+    out_images_dir = os.path.join(outdir, "images")
     train_path = os.path.join(outdir, "train.txt")
     valid_path = os.path.join(outdir, "valid.txt")
-    test_path  = os.path.join(outdir, "text.txt")
+    test_path  = os.path.join(outdir, "test.txt")
     label_path = os.path.join(outdir, "label.txt")
+
+    if not os.path.exists(out_images_dir):
+        os.mkdir(out_images_dir)
 
     with open(train_path, 'w') as train_f, open(valid_path, 'w') as valid_f, open(test_path, 'w') as test_f, open(label_path, 'w') as label_f:
         for i, label in enumerate(labels):
-            images = glob.glob(os.path.join(dir, label, '*'))
-            (train_images, valid_images, test_images) = random_split(images)
+            src_images = glob.glob(os.path.join(dir, label, '*.jpg'))
+            (train_images, valid_images, test_images) = random_split(src_images)
 
-            for path in train_images:
-                train_f.write("{} {}\n".format(path, i))
-            for path in valid_images:
-                valid_f.write("{} {}\n".format(path, i))
-            for path in test_images:
-                test_f.write("{} {}\n".format(path, i))
+            for f, images in ((train_f, train_images), (valid_f, valid_images), (test_f, test_images)):
+                for src_path in images:
+                    basename = os.path.basename(src_path)
+                    dst_path = os.path.join(outdir, 'images', "{}_{}".format(label, basename))
+
+                    f.write("{} {}\n".format(dst_path, i))
+                    shutil.copyfile(src_path, dst_path)
             label_f.write("%d %s\n" % (i, label))
 
 if __name__ == '__main__':
